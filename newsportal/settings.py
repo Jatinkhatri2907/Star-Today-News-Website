6834c8ad6f0b4e4f48fd1f2c9cde517e05a089e1
Jatin Kaushal <jatinkaushal@Jatins-MacBook-Air.local>
2026-03-12 23:53:29 +0530

    Fix allowed hosts

--- newsportal/settings.py
+++ newsportal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)2fkm*@3yw9vx2qta$nmspk(ya=hv9brjcp+jf$oqai&r7dw&j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
